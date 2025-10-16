55e4334711fe17a1ec1c545eca6b19ae71019ff1
Ezekiel-Turnbough03 <ezekielturnbough@gmail.com>
2025-10-16 17:30:46 -0400

    I added the skeleton for navigating between screens, as well as updating the screens files

--- app/src/main/java/edu/gvsu/cis/jafarovn/fishingandroidapp/MainActivity.kt
+++ app/src/main/java/edu/gvsu/cis/jafarovn/fishingandroidapp/MainActivity.kt
@@ -4,14 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import edu.gvsu.cis.jafarovn.fishingandroidapp.screens.AddFishScreen
+import edu.gvsu.cis.jafarovn.fishingandroidapp.screens.HomeScreen
+import edu.gvsu.cis.jafarovn.fishingandroidapp.screens.LeaderBoardScreen
+import edu.gvsu.cis.jafarovn.fishingandroidapp.screens.UserProfileScreen
 import edu.gvsu.cis.jafarovn.fishingandroidapp.ui.theme.FishingAndroidAppTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +21,27 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FishingAndroidAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val navigation_controller = rememberNavController()
+                val mapViewModel: MapViewModel = viewModel()
+                NavHost(navigation_controller, startDestination = "home_screen"){
+                    composable ("home_screen")
+                    {
+                        HomeScreen(mapViewModel = mapViewModel, onNavigateToCaughtFish = {navigation_controller.navigate("add_fish_screen")})
+                    }
+                    composable ("add_fish_screen")
+                    {
+                        AddFishScreen(mapViewModel = mapViewModel, onNavigateToMain= {navigation_controller.navigate("home_screen")})
+                    }
+                    composable ("leaderboard_screen")
+                    {
+                        LeaderBoardScreen(mapViewModel = mapViewModel, onNavigateToMain = {navigation_controller.navigate("home_screen")})
+                    }
+                    composable ("profile_screen")
+                    {
+                        UserProfileScreen(mapViewModel = mapViewModel, onNavigateToMain= {navigation_controller.navigate("home_screen")})
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FishingAndroidAppTheme {
-        Greeting("Android")
     }
 }
