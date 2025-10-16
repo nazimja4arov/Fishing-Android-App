945a87a6a0459169b8605587a2c43a660de38dc4
walhak <djafarovnazim@gmail.com>
2025-10-16 18:06:15 -0400

    Commit before merging with Zeke to merge NavHost

--- app/src/main/java/edu/gvsu/cis/jafarovn/fishingandroidapp/MainActivity.kt
+++ app/src/main/java/edu/gvsu/cis/jafarovn/fishingandroidapp/MainActivity.kt
@@ -4,14 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import edu.gvsu.cis.jafarovn.fishingandroidapp.ui.theme.FishingAndroidAppTheme
+import edu.gvsu.cis.jafarovn.fishingandroidapp.ui.theme.BackgroundImageScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,16 +24,22 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FishingAndroidAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                BackgroundImageScreen()
+
+
+
+
+
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
                 }
             }
         }
     }
-}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
--- app/src/main/java/edu/gvsu/cis/jafarovn/fishingandroidapp/ui/theme/HomeScreen.kt
+++ app/src/main/java/edu/gvsu/cis/jafarovn/fishingandroidapp/ui/theme/HomeScreen.kt
@@ -1,2 +1,44 @@
 package edu.gvsu.cis.jafarovn.fishingandroidapp.ui.theme
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import edu.gvsu.cis.jafarovn.fishingandroidapp.R
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+//import androidx.compose.runtime.R
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun BackgroundImageScreen() {
+    Box(
+        Modifier
+            .fillMaxSize()
+            .paint(
+                painterResource(R.drawable.michigan_counties_map),
+                contentScale = ContentScale.Crop
+            )
+    )
+}
+
+//class HomeScreen : ComponentActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+//        setContent {
+//            FishingAndroidAppTheme {
+//                BackgroundImageScreen(
+//                    modifier = Modifier.padding(horizontal = 0.dp, vertical = 0.dp)
+//                )
+//            }
+//        }
+//    }
+//}
