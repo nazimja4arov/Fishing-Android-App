adac6af086c8588da51e200d2cc1881c74b66a6a
tarabarnett <taralynnbarnett00@gmail.com>
2025-10-19 11:10:03 -0400

    Added curly bracket

--- app/src/main/java/edu/gvsu/cis/jafarovn/fishingandroidapp/MainActivity.kt
+++ app/src/main/java/edu/gvsu/cis/jafarovn/fishingandroidapp/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-<<<<<<< HEAD
+//<<<<<<< HEAD
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -37,36 +37,42 @@ class MainActivity : ComponentActivity() {
                 BackgroundImageScreen()
 
 
-
-
-
 //                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
 //                    Greeting(
 //                        name = "Android",
 //                        modifier = Modifier.padding(innerPadding)
 //                    )
-                
 
-		val navigation_controller = rememberNavController()
+
+                val navigation_controller = rememberNavController()
                 val mapViewModel: MapViewModel = viewModel()
-                NavHost(navigation_controller, startDestination = "home_screen"){
-                    composable ("home_screen")
+                NavHost(navigation_controller, startDestination = "home_screen") {
+                    composable("home_screen")
                     {
-                        HomeScreen(mapViewModel = mapViewModel, onNavigateToCaughtFish = {navigation_controller.navigate("add_fish_screen")})
+                        HomeScreen(
+                            mapViewModel = mapViewModel,
+                            onNavigateToCaughtFish = { navigation_controller.navigate("add_fish_screen") })
                     }
-                    composable ("add_fish_screen")
+                    composable("add_fish_screen")
                     {
-                        AddFishScreen(mapViewModel = mapViewModel, onNavigateToMain= {navigation_controller.navigate("home_screen")})
+                        AddFishScreen(
+                            mapViewModel = mapViewModel,
+                            onNavigateToMain = { navigation_controller.navigate("home_screen") })
                     }
-                    composable ("leaderboard_screen")
+                    composable("leaderboard_screen")
                     {
-                        LeaderBoardScreen(mapViewModel = mapViewModel, onNavigateToMain = {navigation_controller.navigate("home_screen")})
+                        LeaderBoardScreen(
+                            mapViewModel = mapViewModel,
+                            onNavigateToMain = { navigation_controller.navigate("home_screen") })
                     }
-                    composable ("profile_screen")
+                    composable("profile_screen")
                     {
-                        UserProfileScreen(mapViewModel = mapViewModel, onNavigateToMain= {navigation_controller.navigate("home_screen")})
+                        UserProfileScreen(
+                            mapViewModel = mapViewModel,
+                            onNavigateToMain = { navigation_controller.navigate("home_screen") })
                     }
                 }
             }
         }
     }
+}
